9e1b1b0041d5e35b913cb4b29746e3d8441c6537
Jothcv <129566698+Jothcv@users.noreply.github.com>
2024-03-06 02:00:16 -0600

    javascript
    ja

---
+++ script.js
@@ -0,0 +1,17 @@
+//suma de dos numeros 
+//var num1 = prompt("primer numero");
+//var num2 = prompt("segundo numero");
+//var suma 
+//document.write("la suma es:");
+//suma =(parseInt(num1) + parseInt(num2));
+//document.write (suma);
+
+//sacar promedio 
+var num1 = parseInt(prompt("primer numero "));
+var num2 = parseInt(prompt("segundo numero "));
+var num3 = parseInt(prompt("tercero numerp "));
+var num4 = parseInt(prompt("cuarto numero "));
+var suma
+document.write ("el promedio es: ");
+ suma = (parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4));
+document.write (suma/4);
