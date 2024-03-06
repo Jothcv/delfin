ff617bb5e37aacc4a756b6e70a80fa6a9f933829
Jothcv <129566698+Jothcv@users.noreply.github.com>
2024-03-06 08:57:49 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -7,11 +7,27 @@
 //document.write (suma);
 
 //sacar promedio 
-var num1 = parseInt(prompt("primer numero "));
-var num2 = parseInt(prompt("segundo numero "));
-var num3 = parseInt(prompt("tercero numerp "));
-var num4 = parseInt(prompt("cuarto numero "));
+//var num1 = parseInt(prompt("primer numero "));
+//var num2 = parseInt(prompt("segundo numero "));
+//var num3 = parseInt(prompt("tercero numerp "));
+//var num4 = parseInt(prompt("cuarto numero "));
+//var suma
+//document.write ("el promedio es: ");
+// suma = (parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4));
+//document.write (suma/4);
+
+//minutos a segundos
+//var num1 = prompt("minuto")
+//document.write("en segundos es: ");
+//suma = (parseInt(num1)) 
+//document.write (suma*60)
+
+//pedir el ingreso de numero , desenas y centenas etc
+var num1 = parseInt(prompt("ingrese una cantidad de dos digitos"))
+var num2 = parseInt()
 var suma
-document.write ("el promedio es: ");
- suma = (parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4));
-document.write (suma/4);
+suma = (parseInt(num1));
+document.write ("decenas son: ");
+document.write ( suma=num1 );
+document.write ("unidades son: ");
+document.write (suma/10)
