ac5d0743ed7e45a44ab0754cfff680da682718c8
Jothcv <129566698+Jothcv@users.noreply.github.com>
2024-03-06 13:46:58 -0600

    Add files via upload

--- script1.js
+++ script1.js
@@ -24,52 +24,105 @@
 //pedir el ingreso de dos numeros y mostrar si son multiplos o no 
 //pedir el ingreso de 3 notas sacar el promedio  y mostrar si aprobo o reprobo el cuarso 
 
-var mez = prompt("mez")
-var dia = prompt("dia")
-if ((mez=="marzo") && (dia<=21)|| (mez=="abril") && (dia<=19))
+
+
+//ejercicio 1
+
+//var mez = prompt("mez")
+//var dia = prompt("dia")
+//if ((mez=="marzo") && (dia<=21)|| (mez=="abril") && (dia<=19))
+//{
+//    document.write("eres aries")
+//}
+//else if ((mez=="abirl") && (dia>=20) || (mez=="mayo") && (dia<=20))
+//{
+//    document.write("tauro")
+//}
+//else if ((mez=="mayo") && (dia>=21) || (mez=="junio") && (dia<=20) )
+//{
+//document.write("geminis")
+//}
+//else if ((mez=="junio") && (dia>=21) || (mez=="julio") && (dia<=22) )
+//{
+//document.write("cancer")
+//}
+//else if ((mez=="julio") && (dia>=23) || (mez=="agosto") && (dia<=22) )
+//{
+//document.write("leo")
+//}
+//else if ((mez=="agosto") && (dia>=23) || (mez=="septiembre") && (dia<=22) )
+//{
+//document.write("virgo")
+//}
+//else if ((mez=="septiembre") && (dia>=23) || (mez=="octubre") && (dia<=22) )
+//{
+//document.write("libra")
+//}
+//else if ((mez=="octubre" ) && (dia>=23) || (mez=="noviembre") && (dia<=21) )
+//{
+//document.write("escorpio")
+//}
+//else if ((mez=="noviembre") && (dia>=22) || (mez=="diciembre") && (dia<=21) )
+//{
+//document.write("sagitario")
+//}
+//else if ((mez=="diciembre") && (dia>=22) || (mez=="enero") && (dia<=19) )
+//{
+//document.write("capricornio")
+//}
+//else if ((mez=="enero") && (dia>=20) || (mez=="febrero") && (dia<=18) )
+//{
+//document.write("acuario")
+//}
+
+//ejercicio 2
+/*
+var uno=prompt("primer numero")
+var dos=prompt("segundo numero")
+var tres=prompt("tercer numero")
+
+
+
+
+
+
+if( uno<dos && dos<tres)
 {
-    document.write("eres aries")
+    document.write("el mayor es: "+ tres)
+    document.write("el intermedio: "+dos)
+    document.write("el menor: "+uno)
 }
-else if ((mez=="abirl") && (dia>=20) || (mez=="mayo") && (dia<=20))
-{
-    document.write("tauro")
-}
-else if ((mez=="mayo") && (dia>=21) || (mez=="junio") && (dia<=20) )
-{
-document.write("geminis")
-}
-else if ((mez=="junio") && (dia>=21) || (mez=="julio") && (dia<=22) )
-{
-document.write("cancer")
-}
-else if ((mez=="julio") && (dia>=23) || (mez=="agosto") && (dia<=22) )
-{
-document.write("leo")
-}
-else if ((mez=="agosto") && (dia>=23) || (mez=="septiembre") && (dia<=22) )
-{
-document.write("virgo")
-}
-else if ((mez=="septiembre") && (dia>=23) || (mez=="octubre") && (dia<=22) )
-{
-document.write("libra")
-}
-else if ((mez=="octubre" ) && (dia>=23) || (mez=="noviembre") && (dia<=21) )
-{
-document.write("escorpio")
-}
-else if ((mez=="noviembre") && (dia>=22) || (mez=="diciembre") && (dia<=21) )
-{
-document.write("sagitario")
-}
-else if ((mez=="diciembre") && (dia>=22) || (mez=="enero") && (dia<=19) )
-{
-document.write("capricornio")
-}
-else if ((mez=="enero") && (dia>=20) || (mez=="febrero") && (dia<=18) )
-{
-document.write("acuario")
-}
+else if( uno>dos && dos>tres){
+
+    document.write("el mayor es"+uno)
+    document.write("el intermedio"+dos)
+    document.write("el menor "+tres)}
+
+else if( dos<uno && uno<tres){
+
+    document.write("el mayor es"+tres)
+    document.write("el intermedio"+uno)
+    document.write("el menor "+dos)}
+
+else if( dos>tres && tres>uno){
+
+   document.write("el mayor es"+dos)
+   document.write("el intermedio"+tres)
+   document.write("el menor "+uno)}
+
+else if( uno>tres && tres>dos){
+   document.write("el mayor es: "+uno)
+   ocument.write("el intermedio: "+tres)
+   document.write("el menor: "+dos)}
+
+else if( dos>uno && uno>tres){
+   document.write("el mayor es: "+dos)
+   document.write("el intermedio; "+uno)
+   document.write("el menor "+tres)}
+*/
+
+//ejercicio 3
+
 
 
 
